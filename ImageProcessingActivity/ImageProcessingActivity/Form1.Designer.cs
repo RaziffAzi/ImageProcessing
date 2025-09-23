2727761098992fe32c1c3b3ccb61b2ab1eb40d27
Raziff D. Gumapon <150051046+RaziffAzi@users.noreply.github.com>
2025-09-23 15:42:38 +0800

    Add files via upload

---
+++ ImageProcessingActivity/ImageProcessingActivity/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ImageProcessingActivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbImageProcess = new System.Windows.Forms.ComboBox();
+            this.orig = new System.Windows.Forms.PictureBox();
+            this.processed = new System.Windows.Forms.PictureBox();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.btnProcessImage = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnSelectImage2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.orig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbImageProcess
+            // 
+            this.cbImageProcess.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbImageProcess.FormattingEnabled = true;
+            this.cbImageProcess.Items.AddRange(new object[] {
+            "Copy",
+            "Greyscale",
+            "Inversion",
+            "Histogram",
+            "Sepia"});
+            this.cbImageProcess.Location = new System.Drawing.Point(21, 18);
+            this.cbImageProcess.Name = "cbImageProcess";
+            this.cbImageProcess.Size = new System.Drawing.Size(121, 28);
+            this.cbImageProcess.TabIndex = 0;
+            this.cbImageProcess.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // orig
+            // 
+            this.orig.Location = new System.Drawing.Point(39, 72);
+            this.orig.Name = "orig";
+            this.orig.Size = new System.Drawing.Size(500, 500);
+            this.orig.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orig.TabIndex = 1;
+            this.orig.TabStop = false;
+            // 
+            // processed
+            // 
+            this.processed.Location = new System.Drawing.Point(594, 72);
+            this.processed.Name = "processed";
+            this.processed.Size = new System.Drawing.Size(500, 500);
+            this.processed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.processed.TabIndex = 2;
+            this.processed.TabStop = false;
+            this.processed.Click += new System.EventHandler(this.processed_Click);
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(184, 590);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(194, 39);
+            this.btnSelectImage.TabIndex = 3;
+            this.btnSelectImage.Text = "Select Image";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnProcessImage
+            // 
+            this.btnProcessImage.Location = new System.Drawing.Point(644, 18);
+            this.btnProcessImage.Name = "btnProcessImage";
+            this.btnProcessImage.Size = new System.Drawing.Size(194, 39);
+            this.btnProcessImage.TabIndex = 4;
+            this.btnProcessImage.Text = "Process Image";
+            this.btnProcessImage.UseVisualStyleBackColor = true;
+            this.btnProcessImage.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // picture2
+            // 
+            this.picture2.Location = new System.Drawing.Point(1128, 72);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(500, 500);
+            this.picture2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture2.TabIndex = 5;
+            this.picture2.TabStop = false;
+            this.picture2.Click += new System.EventHandler(this.picture2_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(864, 18);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(194, 39);
+            this.btnSubtract.TabIndex = 6;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // btnSelectImage2
+            // 
+            this.btnSelectImage2.Location = new System.Drawing.Point(1311, 590);
+            this.btnSelectImage2.Name = "btnSelectImage2";
+            this.btnSelectImage2.Size = new System.Drawing.Size(194, 39);
+            this.btnSelectImage2.TabIndex = 7;
+            this.btnSelectImage2.Text = "Select Image";
+            this.btnSelectImage2.UseVisualStyleBackColor = true;
+            this.btnSelectImage2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1651, 636);
+            this.Controls.Add(this.btnSelectImage2);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.btnProcessImage);
+            this.Controls.Add(this.btnSelectImage);
+            this.Controls.Add(this.processed);
+            this.Controls.Add(this.orig);
+            this.Controls.Add(this.cbImageProcess);
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.orig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbImageProcess;
+        private System.Windows.Forms.PictureBox orig;
+        private System.Windows.Forms.PictureBox processed;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.Button btnProcessImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnSelectImage2;
+    }
+}
+
---
+++ ImageProcessingActivity/ImageProcessingActivity/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace ImageProcessingActivity
+{
+    public partial class Form1 : Form
+    {
+        Bitmap imageA, imageB;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCaptureWebcam_Click(object sender, EventArgs e)
+        {
+            //IDataObject data;
+            //Image bmap;
+            //Device d = DeviceManager.GetDevice(listBox1.SelectedIndex);
+            //d.Sendmessage();
+            //data = Clipboard.GetDataObject();
+            //bmap = (Image)(data.GetData("System.Drawing.Bitmap", true));
+            //Bitmap b = new Bitmap(bmap);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            openFileDialog1.Title = "Select an image";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    imageA = new Bitmap(openFileDialog1.FileName);
+                    orig.Image = imageA; // show it in PictureBox (optional)
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Invalid image file: " + ex.Message);
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (orig.Image == null)
+            {
+                MessageBox.Show("Please select an image first.");
+                return;
+            }
+            if(cbImageProcess.SelectedItem == null)
+            {
+                MessageBox.Show("Please select an image processing method.");
+                return;
+            }
+            switch (cbImageProcess.SelectedItem.ToString())
+            {
+
+                case "Copy":
+                    processed.Image = (Image)orig.Image.Clone();
+                    break;
+
+                case "Greyscale":
+                    Greyscale();
+                    break;
+
+                case "Inversion":
+                    Inversion();
+                    break;
+
+                case "Histogram":
+                    Histogram();
+                    break;
+
+                case "Sepia":
+                    Sepia();
+                    break;
+            }
+        }
+        
+        private void Greyscale()
+        {
+            Bitmap bp = new Bitmap(orig.Image.Width, orig.Image.Height);
+            for (int i = 0; i < orig.Image.Width; i++)
+            {
+                for (int j = 0; j < orig.Image.Height; j++)
+                {
+                    Color c = ((Bitmap)orig.Image).GetPixel(i, j);
+                    int gray = ((c.R + c.G + c.B) / 3);
+                    bp.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                }
+            }
+            processed.Image = bp;
+        }
+
+        private void Inversion()
+        {
+            Bitmap bp = new Bitmap(orig.Image.Width, orig.Image.Height);
+            for (int i = 0; i < orig.Image.Width; i++)
+            {
+                for (int j = 0; j < orig.Image.Height; j++)
+                {
+                    Color c = ((Bitmap)orig.Image).GetPixel(i, j);
+                    bp.SetPixel(i, j, Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B));
+                }
+            }
+            processed.Image = bp;
+        }
+
+        private void Sepia()
+        {
+            Bitmap bp = new Bitmap(orig.Image.Width, orig.Image.Height);
+            for (int i = 0; i < orig.Image.Width; i++)
+                    {
+                        for (int j = 0; j < orig.Image.Height; j++)
+                        {
+                            Color c = ((Bitmap)orig.Image).GetPixel(i, j);
+                            int tr = (int)(0.393 * c.R + 0.769 * c.G + 0.189 * c.B);
+                            int tg = (int)(0.349 * c.R + 0.686 * c.G + 0.168 * c.B);
+                            int tb = (int)(0.272 * c.R + 0.534 * c.G + 0.131 * c.B);
+
+                            if(tr > 255)
+                            {
+                                tr = 255;
+                            }
+                            if(tg > 255)
+                            {
+                                tg = 255;
+                            }
+                            if (tb > 255)
+                            {
+                                tb = 255;
+                            }
+                            bp.SetPixel(i,j, Color.FromArgb(tr, tg, tb));
+                        }
+                    }
+                    processed.Image = bp;
+            processed.Image = bp;
+        }
+
+        public void Histogram()
+        {
+            int[] arr = new int[256];
+            Bitmap bp = new Bitmap(orig.Image.Width, orig.Image.Height);
+            for (int i = 0; i < orig.Image.Width; i++)
+            {
+                for (int j = 0; j < orig.Image.Height; j++)
+                {
+                    Color c = ((Bitmap)orig.Image).GetPixel(i, j);
+                    int gray = ((c.R + c.G + c.B) / 3);
+                    arr[gray]++;
+                    //bp.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                }
+            }
+            int max = arr.Max();
+            Graphics g = Graphics.FromImage(bp);
+            for (int i = 0; i < 256; i++)
+            {
+                int scaledHeight = (arr[i] * bp.Height) / max;
+                int x = (i * bp.Width) / 256;
+                g.DrawLine(Pens.Black, x, bp.Height, x, bp.Height - scaledHeight);
+            }
+            processed.Image = bp;
+        }
+
+        private void processed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picture2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //load image2
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            openFileDialog1.Title = "Select an image";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    imageB = new Bitmap(openFileDialog1.FileName);
+                    picture2.Image = imageB; // show it in PictureBox (optional)
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Invalid image file: " + ex.Message);
+                }
+            }
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            //subtract
+            processed.Image = null;
+            if (imageA.Width != imageB.Width || imageA.Height != imageB.Height)
+            {
+                imageB = new Bitmap(imageB, new Size(imageA.Width, imageA.Height));
+            }
+            Color mygreen = Color.FromArgb(0, 255, 0); 
+            int threshold = 5; 
+            int greygreen = (mygreen.R + mygreen.G + mygreen.B) / 3;
+            Bitmap resultImage = new Bitmap(imageA.Width, imageA.Height);
+
+            for (int x = 0; x < imageA.Width; x++)
+            {
+                for (int y = 0; y < imageA.Height; y++)
+                {
+                    Color pixel = imageA.GetPixel(x, y);
+                    Color backpixel = imageB.GetPixel(x, y);
+
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    
+                    int subtractvalue = Math.Abs(grey-greygreen);
+
+                    if (subtractvalue < threshold)
+                        resultImage.SetPixel(x, y, backpixel);
+                    else
+                        resultImage.SetPixel(x, y, pixel);
+                }
+            }
+
+            processed.Image = resultImage;
+        }
+    }
+}
